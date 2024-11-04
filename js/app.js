f071fb9e867166a6e955155b66a3ebb96eb5024d
Tchichem <152623185+Tchichem@users.noreply.github.com>
2024-11-04 10:10:21 +0100

    initializing

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// Exercice : Jeu du nombre secret
+// Objectif
+// Le joueur doit deviner un nombre aléatoire entre 1 et 10. À chaque essai, le programme donnera un indice (trop haut, trop bas, ou correct). Le jeu doit permettre de relancer une partie en appuyant sur une touche.
+
+// Étapes
+// Variables et constantes : Créez des constantes pour les messages d'indice (par exemple, "Trop haut", "Trop bas", "Correct") et une variable pour stocker le nombre d'essais restants.
+// Conditions et boucles : Utilisez une boucle pour permettre à l'utilisateur de faire plusieurs essais jusqu'à ce qu'il devine le nombre ou que les essais soient épuisés.
+// Méthode pour générer un nombre aléatoire.
+// Méthode pour vérifier la réponse et retourner le bon message.
+// Bonus : Affichez les essais restants après chaque tentative.
+
+// Mega bonus : 3 essais
